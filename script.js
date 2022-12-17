e6d83d7c09ed7f6d9ae5ba791020d4382a2221ef
rafael <rafadev25@gmail.com>
2022-12-16 21:19:14 -0300

    Pequeno ajuste de css e calculo de imc

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function calcImc(){
-    alert('em construção');
+    // Calculo do imc = 
+    let nome = document.getElementById('name').value;
+    let nascimento = new Date(document.getElementById('birthday').value);
+    let hoje = new Date();
+    let idade = hoje.getFullYear() - nascimento.getFullYear();
+    let peso = document.getElementById('weight').value;
+    let altura = document.getElementById('height').value/100;
+
+    let imc = peso / (altura * altura);
+    let mensagem = `Olá ${nome}, você tem ${idade} anos e seu IMC é ${imc.toFixed(1)}`;
+
+    alert(mensagem);
+    
 }
